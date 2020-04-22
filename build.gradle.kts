81ebcdcd7757d21977ab00e295b467ffb2f450a8
Alexandre Lombard <alexandre.lombard@utbm.fr>
2020-04-22 14:40:36 +0200

    Updating multi-module configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,46 +1,55 @@
-plugins {
-    kotlin("multiplatform") version "1.3.72"
-}
+import java.util.Properties
 
-group = "org.apache.commonskt"
-version = "1.0-SNAPSHOT"
+plugins {
+    id("base")
+    kotlin("multiplatform") version "1.3.72"
+    id("maven-publish")
+    id("com.jfrog.bintray") version "1.8.5"
+}
 
 repositories {
     mavenLocal()
     mavenCentral()
 }
 
-kotlin {
-    jvm()
-    js()
-    mingwX64()
-    linuxX64()
+allprojects {
+    group = "org.apache.commonskt.numbers"
+    version = "1.0.0-SNAPSHOT"
 
-    sourceSets {
-        val commonMain by getting {
-            dependencies {
-                implementation(kotlin("stdlib-common"))
-                implementation("org.apache.commonskt:kotlin-stdlib-extension:1.0-SNAPSHOT")
-            }
-        }
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test-common"))
-                implementation(kotlin("test-annotations-common"))
-                implementation("org.apache.commonskt:kotlin-stdlib-extension:1.0-SNAPSHOT")
-            }
-        }
-        val jvmMain by getting {
-            dependencies {
-                implementation(kotlin("stdlib"))
-                implementation("org.apache.commonskt:kotlin-stdlib-extension-jvm:1.0-SNAPSHOT")
-            }
-        }
-        val jsMain by getting {
-            dependencies {
-                implementation(kotlin("stdlib-js"))
-                implementation("org.apache.commonskt:kotlin-stdlib-extension-js:1.0-SNAPSHOT")
-            }
-        }
+    repositories {
+        jcenter()
+    }
+}
+
+val localProperties = Properties()
+localProperties.load(project.rootProject.file("local.properties").inputStream())
+
+subprojects {
+    val subproject = this@subprojects
+
+    apply(plugin = "maven-publish")
+    apply(plugin = "com.jfrog.bintray")
+
+    bintray {
+        user = localProperties.getProperty("bintray.user")
+        key = localProperties.getProperty("bintray.apikey")
+        dryRun = false
+        publish = true
+        val pubs = publishing.publications
+            .map { it.name }
+            .filter { it != "kotlinMultiplatform" }
+            .toTypedArray()
+        setPublications(*pubs)
+        pkg(delegateClosureOf<com.jfrog.bintray.gradle.BintrayExtension.PackageConfig> {
+            repo = "maven"
+            name = subproject.name
+            desc = project.description
+            githubRepo = "https://github.com/alexandrelombard/commonskt-numbers"
+            websiteUrl = "https://github.com/alexandrelombard/commonskt-numbers"
+            vcsUrl = "https://github.com/alexandrelombard/commonskt-numbers.git"
+            version.vcsTag = "v${project.version}"
+            setLicenses("Apache-2.0")
+            publicDownloadNumbers = true
+        })
     }
 }
